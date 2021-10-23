01c9e754974a3e51a33c9cf1fe3348538e3fbc0c
myagkov_k <myagkov_k@webant.ru>
2021-10-23 19:19:04 +0300

    Добавлен ConfirmationFragment

--- app/src/main/java/com/example/app/ui/confirmation/ConfirmationFragment.kt
+++ app/src/main/java/com/example/app/ui/confirmation/ConfirmationFragment.kt
@@ -1,4 +1,16 @@
 package com.example.app.ui.confirmation
 
-class ConfirmationFragment {
+import com.airbnb.mvrx.fragmentViewModel
+import com.example.app.databinding.FragmentConfirmationBinding
+import com.example.app.ui.base.BaseFragment
+
+
+class ConfirmationFragment : BaseFragment<FragmentConfirmationBinding>()  {
+
+    override val viewModel: ConfirmationViewModel by fragmentViewModel()
+
+
+    override fun getBinding(): FragmentConfirmationBinding {
+        return FragmentConfirmationBinding.inflate(layoutInflater)
+    }
 }
--- app/src/main/java/com/example/app/ui/confirmation/ConfirmationViewModel.kt
+++ app/src/main/java/com/example/app/ui/confirmation/ConfirmationViewModel.kt
@@ -1,4 +1,26 @@
+@file:Suppress("MaxLineLength")
 package com.example.app.ui.confirmation
 
-class ConfirmationViewModel {
+import com.airbnb.mvrx.MavericksViewModelFactory
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
+import com.example.app.di.viewmodel.AssistedViewModelFactory
+import com.example.app.di.viewmodel.hiltMavericksViewModelFactory
+import com.example.app.ui.base.BaseViewModel
+import com.example.app.ui.base.BaseViewState
+
+data class ConfirmationFragmentViewState(
+    val a: Int = 0
+): BaseViewState
+
+class ConfirmationViewModel @AssistedInject constructor(
+    @Assisted state: ConfirmationFragmentViewState
+): BaseViewModel<ConfirmationFragmentViewState, ConfirmationFragmentViewEvents, ConfirmationFragmentViewActions>(state) {
+
+    @AssistedFactory
+    interface Factory : AssistedViewModelFactory<ConfirmationViewModel, ConfirmationFragmentViewState>
+
+
+    companion object : MavericksViewModelFactory<ConfirmationViewModel, ConfirmationFragmentViewState> by hiltMavericksViewModelFactory()
 }
